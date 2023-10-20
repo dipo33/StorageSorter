e2fe894af7681a4f0adf97b5bb42139a1629efef
dipo33 <mato.dipcar@gmail.com>
2023-10-20 03:24:41 +0200

    refactor: rename main mod class

--- src/main/java/com/dipo33/storagesorter/StorageSorterMod.java
+++ src/main/java/com/dipo33/storagesorter/StorageSorterMod.java
@@ -30,8 +30,8 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(StorageSorterMod.MODID)
+public class StorageSorterMod
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "storage_sorter";
@@ -61,7 +61,7 @@ public class ExampleMod
                 output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
-    public ExampleMod()
+    public StorageSorterMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
